71575339baefadf9bb2589c7b2ba561a3f5f30c3
Alex Bessa <alexbessa.dev@gmail.com>
2026-07-17 21:40:12 -0300

    refactor: mover dados simulados para diretorio dedicado

---
+++ src/mocks/customer.mock.ts
@@ -0,0 +1,17 @@
+import type { Customer } from "../types.ts";
+
+const customers: Customer[] = [{
+    id: 1,
+    name: 'Alex Bessa',
+    status: true
+}, {
+    id: 2,
+    name: 'Valdiana Bessa',
+    status: true
+}, {
+    id: 3,
+    name: 'Ana Bessa',
+    status: false
+}];
+
+export default customers;
