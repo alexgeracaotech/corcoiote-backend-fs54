7a8f9c3ba6021e81480533209949bc39dfb0b5b3
Alex Bessa <alexbessa.dev@gmail.com>
2026-07-31 21:37:16 -0300

    feat: adicionar intermediarios para registro de requisicoes e menipulacao de erros

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
 import express from 'express';
+import errorHandler from './middlewares/errorHandler.ts';
+import requestLogger from './middlewares/requestLogger.ts';
 import CustomerRouter from './routes/customer.route.ts';
 
 const app = express();
 
+app.use(requestLogger);
+
 app.use(express.json());
 
 app.use('/customers', CustomerRouter);
+
+app.use(errorHandler);
 
 app.listen(3000);
